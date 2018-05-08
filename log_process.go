b8dc00e8e078085fb1a1a7cd1bf303243059f55d
dylan <dylangeng@163.com>
2018-05-08 13:08:29 +0800

    change log_process.go basic flow

--- log_process.go
+++ log_process.go
@@ -1,10 +1,48 @@
 package main
 
+import (
+	"strings"
+	"fmt"
+	"time"
+)
+
 type LogProcess struct {
-	path string
-	dns string
+	rc chan string
+	wc chan string
+	path string //file path
+	influxDBDsn string // influx data source
+}
+
+func (l *LogProcess) ReadFromFile(){
+	//read module
+	l.rc <- "Hello, Dylan"
+}
+
+func (l *LogProcess) Process(){
+	//process
+	ct := <- l.rc
+	l.wc <- strings.ToUpper(ct)
+}
+
+func (l *LogProcess) WriteToInfluxDB(){
+	//write module
+	fmt.Println(<-l.wc)
 }
 
 func main() {
-	
+	lp := &LogProcess{
+		rc: make(chan string),
+		wc: make(chan string),
+		path: ".\\access.log",
+		influxDBDsn: "username&password..",
+	}
+
+	go lp.ReadFromFile()
+
+	go lp.Process()
+
+	go lp.WriteToInfluxDB()
+
+	time.Sleep(1*time.Second)
+
 }
