bf1160a323685f13020ff889f54f76b4d633c1b0
dylan <dylangeng@163.com>
2018-05-08 23:34:44 +0800

    add process module

--- log_process.go
+++ log_process.go
@@ -7,6 +7,10 @@ import (
 	"os"
 	"bufio"
 	"io"
+	"regexp"
+	"log"
+	"strconv"
+	"net/url"
 )
 
 type Reader interface {
@@ -14,7 +18,7 @@ type Reader interface {
 }
 
 type Writer interface {
-	Write(wc chan string)
+	Write(wc chan *Message)
 }
 
 type ReadFromFile struct {
@@ -25,7 +29,14 @@ type WriteToInfluxDB struct {
 	influxDBDsn string
 }
 
-func (w *WriteToInfluxDB) Write(wc chan string) {
+type Message struct {
+	TimeLocal						time.Time
+	BytesSent						int
+	Path, Method, Scheme, Status	string
+	UpstreamTime, RequestTime		float64
+}
+
+func (w *WriteToInfluxDB) Write(wc chan *Message) {
 	//write module
 	for v := range wc {
 		fmt.Println(v)
@@ -60,15 +71,65 @@ func (r *ReadFromFile) Read(rc chan string) {
 
 type LogProcess struct {
 	rc chan string
-	wc chan string
+	wc chan *Message
 	read Reader
 	write Writer
 }
 
 func (l *LogProcess) Process(){
 	//process
+	//从Read Channel中读取每行日志数据
+	//正则来对其进行匹配
+	/**
+	172.0.0.12 -- [04/Mar/2018:13:49:52 +0000] http "GET /foo?query=t HTTP/1.0" 200 2133 "-" "KeepAliveClient" "-"1.005 1.854
+	
+	([\d\.]+)\s+([^ \[]+)\s+([^ \[]+)\s+\[([^\]]+)\]\s+([a-z]+)\s+\"([^"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^"]+)\"\s+\"(.*?)\"\s+\"([\d\.-]+)\"\s+([\d\.-]+)\s+([\d\.-]+)
+	**/
+
+	r := regexp.MustCompile(`([\d\.]+)\s+([^ \[]+)\s+([^ \[]+)\s+\[([^\]]+)\]\s+([a-z]+)\s+\"([^"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^"]+)\"\s+\"(.*?)\"\s+\"([\d\.-]+)\"\s+([\d\.-]+)\s+([\d\.-]+)`)
+
+	location, _ := time.LoadLocation("Asia/Shanghai")
 	for v := range l.rc {
-		l.wc <- strings.ToUpper(v)
+		ret := r.FindStringSubmatch(v)
+		if len(ret) != 14 {
+			log.Println("FindStringSubmatch fail:", v)
+			continue
+		}
+		
+		message := &Message{}
+		t, err := time.ParseInLocation("02/Jan/2006:15:04:05 +0000", ret[4], location)
+
+		if err != nil {
+			log.Println("ParseInLocation fail:", err.Error(), ret[4])
+		}
+		message.TimeLocal = t
+
+		byteSent, _ := strconv.Atoi(ret[8])
+		message.BytesSent = byteSent
+
+		//GET /foo?query=t HTTP/1.0
+		reqSli := strings.Split(ret[6], " ")
+		if len(reqSli) != 3 {
+			log.Println("strings.Split fail", ret[6])
+			continue
+		}
+		message.Method = reqSli[0]
+		u, err := url.Parse(reqSli[1])
+		if err != nil {
+			log.Println("url parse fail:", err)
+			continue
+		}
+		message.Path = u.Path
+
+		message.Scheme = ret[5]
+		message.Status = ret[7]
+
+		upstreamTime, _ := strconv.ParseFloat(ret[12], 64)
+		requestTime, _ := strconv.ParseFloat(ret[13], 64)
+		message.UpstreamTime = upstreamTime
+		message.RequestTime = requestTime
+
+		l.wc <- message
 	}
 }
 
@@ -83,7 +144,7 @@ func main() {
 
 	lp := &LogProcess{
 		rc: make(chan string),
-		wc: make(chan string),
+		wc: make(chan *Message),
 		read: r,
 		write: w,
 	}
